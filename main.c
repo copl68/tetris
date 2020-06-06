7fa8f856455f28a2769af0515983e60f1db6b784
Cole Plum <cplum@udel.edu>
2020-06-05 21:31:08 -0400

    createPiece() started

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sense/sense.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 pi_framebuffer_t *fb;
 int run = 1;
@@ -19,7 +20,43 @@ typedef struct Shape{
 	int color;
 } Shape; 
 
-enum colors{Red, Orange, Yellow, Green, Blue, Purple, White};
+enum colors{Red = 1, Orange, Yellow, Green, Blue, Purple, White};
+
+void createPiece(int pieceNum, Shape *activePiece){
+	for(int i = 0; i < 3; i++){
+		for(int j = 0; j < 3; j++){
+			activePiece->layout[i][j] = 0;
+		}
+	}
+	
+	switch(pieceNum){
+		case 1:
+			//Straight piece
+			activePiece->color = red;
+			activePiece->layout[0][0] = 1;
+			activePiece->layout[0][1] = 1;
+			activePiece->layout[0][2] = 1;
+			break;
+		case 2:
+			activePiece->color = orange;
+			break;
+		case 3:
+			activePiece->color = yellow;
+			break;
+		case 4:
+			activePiece->color = green;
+			break;
+		case 5:
+			activePiece->color = blue;
+			break;
+		case 6:
+			activePiece->color = purple;
+			break;
+		case 7:
+			activePiece->color = white;
+			break;	
+	}
+}
 
 //Used to exit the program when needed
 void interrupt_handler(int sig){
@@ -41,5 +78,7 @@ int main(){
 	white = getColor(255, 255, 255);
 	blank = getColor(0, 0, 0);	
 
-	
+	Shape activePiece;
+	printf("&activePiece: %d\n", &activePiece);
+	createPiece(1, &activePiece);	
 }
